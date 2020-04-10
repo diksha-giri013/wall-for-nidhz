aa0b50788cdf014f216218561169a607327496e5
diksha-giri013 <diksha.giri013@gmail.com>
2020-04-11 02:09:26 +0530

    Updates

---
+++ precache-manifest.2d4c5bed1cba5240c933986a92ff63a9.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "972b229b3bc741a07130e861d6fb4c64",
+    "url": "/wall-for-nidhz/memories/index.html"
+  },
+  {
+    "revision": "f1d8154443ac268112e7",
+    "url": "/wall-for-nidhz/memories/static/css/main.5a087666.chunk.css"
+  },
+  {
+    "revision": "dcc63120013130896644",
+    "url": "/wall-for-nidhz/memories/static/js/2.ec487db3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wall-for-nidhz/memories/static/js/2.ec487db3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1d8154443ac268112e7",
+    "url": "/wall-for-nidhz/memories/static/js/main.158bae81.chunk.js"
+  },
+  {
+    "revision": "effe2e17a1f73bf2a8c1",
+    "url": "/wall-for-nidhz/memories/static/js/runtime-main.1f950100.js"
+  },
+  {
+    "revision": "b775dfca1bdb1d8bdd07ece1f6abbd0a",
+    "url": "/wall-for-nidhz/memories/static/media/Artboard2.b775dfca.png"
+  },
+  {
+    "revision": "b5c08442b270f488072637987edc095e",
+    "url": "/wall-for-nidhz/memories/static/media/Artboard3.b5c08442.png"
+  },
+  {
+    "revision": "4f337330670ff18358a81386b64fa1e1",
+    "url": "/wall-for-nidhz/memories/static/media/Artboard4.4f337330.png"
+  },
+  {
+    "revision": "5e701109373b1bbf071e4cd0eca75a2b",
+    "url": "/wall-for-nidhz/memories/static/media/Artboard5.5e701109.png"
+  },
+  {
+    "revision": "7ff017429187c99441ab7459ecf52b48",
+    "url": "/wall-for-nidhz/memories/static/media/Artboard6.7ff01742.png"
+  },
+  {
+    "revision": "99d61b72f9a5268a4be7a99a5e911bc1",
+    "url": "/wall-for-nidhz/memories/static/media/Artboard7.99d61b72.png"
+  },
+  {
+    "revision": "f0910469ee33e6e484d5af54f27e3098",
+    "url": "/wall-for-nidhz/memories/static/media/Artboard8.f0910469.png"
+  },
+  {
+    "revision": "8caff316048bad2a5171dec80375c091",
+    "url": "/wall-for-nidhz/memories/static/media/back2.8caff316.jpg"
+  },
+  {
+    "revision": "b4544b2d6eb872d427fd6591511872f5",
+    "url": "/wall-for-nidhz/memories/static/media/footer.b4544b2d.jpg"
+  },
+  {
+    "revision": "e6b3910715f99c583e3f8b11373fa539",
+    "url": "/wall-for-nidhz/memories/static/media/section1.e6b39107.jpg"
+  },
+  {
+    "revision": "aca3d6484fb97d9605ff339cf24aa206",
+    "url": "/wall-for-nidhz/memories/static/media/shygirl.aca3d648.mp3"
+  }
+]);
